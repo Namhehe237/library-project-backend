a19ae19b166d717de9db26997ed81459b4232cf9
Namhehe237 <namnguyen23072003@gmail.com>
2024-11-26 22:13:54 +0700

    add Review

---
+++ demo/src/main/java/backend/library/dao/ReviewRepository.java
@@ -0,0 +1,13 @@
+package backend.library.dao;
+
+import backend.library.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByBookId(@RequestParam("book_id")Long bookId, Pageable pageable);
+
+}
